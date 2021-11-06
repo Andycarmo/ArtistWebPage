a0c630212e64b677e9423bbd04657d249fec3470
Andres <83571422+Andycarmo@users.noreply.github.com>
2021-11-06 02:49:30 -0500

    Version 1.0.3

--- src/main/java/com/WebPage/writer/controlador/usuariosControlador.java
+++ src/main/java/com/WebPage/writer/controlador/usuariosControlador.java
@@ -6,10 +6,15 @@ package com.WebPage.writer.controlador;
 
 import com.WebPage.writer.modelo.usuariosModelo;
 import com.WebPage.writer.repositorio.usuariosRepositorio;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,15 +31,30 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/usuarios")
 public class usuariosControlador {
     
+    /// Variable de interfaza de Modelo
     @Autowired
     private usuariosRepositorio usu;
     
+    /// Procedimiento guardar
     @PostMapping("/guardar")
     public usuariosModelo guardarUsuario(@Validated @RequestBody usuariosModelo varU){
-    
         return usu.insert(varU);
     }
     
+    ///Procedimiento consulta general
+    @GetMapping("/consultar")
+    public List<usuariosModelo> consultarUsuarios(){
+        return usu.findAll();
+    }
     
+    /// Procedimiento actualizar
+    @PutMapping("/actualizar/{id}")
+    public usuariosModelo actualizarUsuario(@PathVariable String id, @Validated @RequestBody usuariosModelo varU){
+        return usu.save(varU);
+    }
     
+    /// Procedimiento eliminar usuario
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminarUsuario(@PathVariable String id){
+    }
 }
